8311a5c5685a6faa1128fc619331bd3acf41391a
Anton-Logvinenko <110742030+Anton-Logvinenko@users.noreply.github.com>
2022-12-24 15:18:26 +0000

    Deploying to gh-pages from @ Anton-Logvinenko/goit-js-hw-08@f5ccbbeb6484fbc34ad60af7da26341d5a3a6e09 🚀

--- 03-feedback.99cc3545.js
+++
@@ -1,2 +0,0 @@
-console.log("tawsegs"),console.log("tawsegs");
-//# sourceMappingURL=03-feedback.99cc3545.js.map
--- 03-feedback.e0dbe462.js
+++
@@ -1,2 +0,0 @@
-console.log("tawsegs"),console.log("tawsegs");
-//# sourceMappingURL=03-feedback.e0dbe462.js.map
